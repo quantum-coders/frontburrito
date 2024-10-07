10890a6a528179ddc8bdd0d164f05f3b70e68bf5
syn4ps3 <caballerofermi@gmail.com>
2024-10-07 15:03:59 -0600

    Added updated social links

--- components/Platform/Socials.vue
+++ components/Platform/Socials.vue
@@ -1,75 +1,69 @@
 <template>
-  <div class="socials-container">
-    <ul class="social-icons">
-      <li>
-        <a href="https://x.com/burritoAIcrypto" target="_blank">
-          <img src="/images/icon-x.png" alt="X" />
-        </a>
-      </li>
-      <li>
-        <a href="https://discord.gg/ejp8cEqRNF" target="_blank">
-          <img src="/images/icon-exchange.png" alt="Discord" />
-        </a>
-      </li>
-      <li>
-        <a href="https://t.me/blockchainBurritoAI" target="_blank">
-          <img src="/images/icon-telegram.png" alt="Telegram" />
-        </a>
-      </li>
-      <!-- <li>
-        <a href="#" target="_blank">
-          <img src="/images/icon-international.svg" alt="DexTools" />
-        </a>
-      </li> -->
-    </ul>
-  </div>
+	<div class="socials-container">
+		<ul class="social-icons">
+			<li>
+				<a href="https://x.com/burritoAIDeFi" target="_blank">
+					<img src="/images/icon-x.png" alt="X" />
+				</a>
+			</li>
+			<li>
+				<a href="https://discord.gg/ejp8cEqRNF" target="_blank">
+					<img src="/images/icon-exchange.png" alt="Discord" />
+				</a>
+			</li>
+			<li>
+				<a href="https://t.me/burritoAIDeFi" target="_blank">
+					<img src="/images/icon-telegram.png" alt="Telegram" />
+				</a>
+			</li>
+			<!-- <li>
+				<a href="#" target="_blank">
+					<img src="/images/icon-international.svg" alt="DexTools" />
+				</a>
+			</li> -->
+		</ul>
+	</div>
 </template>
 
-<style lang="scss" scoped>
-  .socials-container {
-    display: flex;
-    justify-content: space-around;
-    align-items: center;
-    margin-top: 2rem;
-    width: 100%;
+<style lang="sass" scoped>
+.socials-container
+	display: flex
+	justify-content: space-around
+	align-items: center
+	margin-top: 2rem
+	width: 100%
 
-    .social-icons {
-      display: flex;
-      list-style: none;
-      padding: 0;
-      margin: 0;
-      gap: 2.5rem;
+	.social-icons
+		display: flex
+		list-style: none
+		padding: 0
+		margin: 0
+		gap: 2.5rem
 
-      li {
-        a {
-          display: inline-block;
+		li
+			a
+				display: inline-block
 
-          img {
-            width: 50px;
-            height: 50px;
-            transition: transform 0.3s ease, opacity 0.3s ease;
+				img
+					width: 30px
+					height: 30px
+					transition: transform 0.3s ease, opacity 0.3s ease
 
-            &:hover {
-              transform: scale(1.1);
-              opacity: 0.8;
-            }
-          }
-        }
-      }
-    }
-  }
+					&:hover
+						transform: scale(1.1)
+						opacity: 0.8
 
-  @media (max-width: 768px) {
-    .social-icons li img {
-      width: 40px;
-      height: 40px;
-    }
-  }
+		@media (min-width: $sm)
+			li
+				a
+					img
+						width: 40px
+						height: 40px
 
-  @media (max-width: 480px) {
-    .social-icons li img {
-      width: 30px;
-      height: 30px;
-    }
-  }
+		@media (min-width: $md)
+			li
+				a
+					img
+						width: 50px
+						height: 50px
 </style>
