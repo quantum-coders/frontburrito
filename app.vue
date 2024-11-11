75816dd780a149d706ff243abc02ecf622714274
0xjesus <pdlgmcn@protonmail.com>
2024-11-11 15:46:18 -0600

    ✨ (app.vue): enhance tracking functionality by adding support for additional tracking parameters like traffic source, utm_medium, and utm_campaign 📝 (app.vue): improve code readability and maintainability by restructuring trackingParams object and adding comments for better understanding

--- app.vue
+++ app.vue
@@ -8,12 +8,23 @@ const router = useRouter()
 
 onMounted(() => {
   const pageName = router.currentRoute.value.name
-  console.info('-----------------------> pageName', pageName)
-
-  useTrackEvent('page_view', {
+  const urlParams = new URLSearchParams(window.location.search)
+  const trackingParams = {
     page_title: document.title,
     page_path: window.location.pathname,
-    page_name: pageName
-  })
+    page_name: pageName,
+    traffic_source: urlParams.get('utm_source') || document.referrer || 'direct'
+  }
+
+  // Solo añade los UTM parameters si existen
+  if (urlParams.get('utm_medium')) {
+    trackingParams.utm_medium = urlParams.get('utm_medium')
+  }
+
+  if (urlParams.get('utm_campaign')) {
+    trackingParams.utm_campaign = urlParams.get('utm_campaign')
+  }
+
+  useTrackEvent('page_view', trackingParams)
 })
 </script>
